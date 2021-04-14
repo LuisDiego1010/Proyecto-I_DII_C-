fa881d045f42b7b8120f560085e26c847f1f2533
LuisDiego <guisogarcia1010@gmail.com>
2021-04-13 18:24:36 -0600

    Create List of data in textedit

--- InterfaceC!/Interface/mainwindow.cpp
+++ InterfaceC!/Interface/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_runbtn_clicked()
+{
+    QString code = ui->stdout->toPlainText();
+    QStringList listcode = code.split(QRegExp("[\n]"),QString::SkipEmptyParts);
+    qInfo( "C Style Info Message" );
 }
