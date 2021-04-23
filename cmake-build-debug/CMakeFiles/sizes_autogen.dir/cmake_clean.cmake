ed829db696280ea6a70847dd21e3f910afcf50e7
isriom <62965230+isriom@users.noreply.github.com>
2021-04-22 23:44:45 -0600

    Delete CMake cache

--- cmake-build-debug/CMakeFiles/sizes_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/sizes_autogen"
-  "sizes_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/sizes_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
